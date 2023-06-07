bb52729f20af1e6d0867dc700b0ce58ba0e738a9
AkinoKimeo <47053804+AkinoKimeo@users.noreply.github.com>
2023-06-07 02:51:27 +0000

    New line changes in printf

--- .ccls-cache/@home@runner@FirstCProgram/main.c
+++ .ccls-cache/@home@runner@FirstCProgram/main.c
@@ -4,7 +4,8 @@
 //function main begins program execution
 int main(void) 
 {
-  printf("Welcome to C!\n");
+  printf("Welcome to C!\nThis is a new line\nMy name is Abraham");
   return(0);
 }
+//return(0); is to signify that it is the end of a function.
 //end function main
